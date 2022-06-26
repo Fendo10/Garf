396f216fef593582cabcedfd5bd06c50024befb0
Fendo10 <104589660+Fendo10@users.noreply.github.com>
2022-06-26 12:02:41 -0500

    Add files via upload

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'GARF';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Here you will find everything you need for your .zip';
+    : 'Created for designers';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
